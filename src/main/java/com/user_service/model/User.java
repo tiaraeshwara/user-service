bdb9997cc6b192523bb83925f80860b0f9458704
tiaraeshwara <tiaraeshwara320@gamil.com>
2026-01-15 17:38:31 +0530

    Created insrt flyway script

--- src/main/java/com/user_service/model/User.java
+++ src/main/java/com/user_service/model/User.java
@@ -1,5 +1,16 @@
 package com.user_service.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class User {
-    
+    private String firstName;
+    private String lastName;  
+    private int age;
+    private String gender;
+    private String city;
+    private String email;
+    private String phoneNumber;
 }
---
+++ src/main/resources/db/migration/V1_1__insert_users.sql
@@ -0,0 +1,4 @@
+INSERT INTO ts.user_info 
+(UserID, LastName, FirstName, Age, Gender, City, PhoneNumber, Email) 
+VALUES
+(1,'Eshwara','Tiara',25,'Female','New York','0710780080','tiaraeshwara@gmail.com');
